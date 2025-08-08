e5fa1bf19f2568af86eeafe59745aa65e58e33e4
Susanna Chapman <susanna.chapman@kraken.tech>
2025-08-08 09:29:24 +0100

    remove comments

--- src/VideoForm.tsx
+++ src/VideoForm.tsx
@@ -61,24 +61,3 @@ export function VideoForm() {
         </form>
     )
 }
-// think about adding validation - maybe
-// might be good one to discuss with Liv later today - data validation - thank you
-
-
-
-// add value and on change to each input
-// fill out inputs
-// once I've connect form fields to formik
-// fill out form, press submit, see values
-// raise PR
-// before I move onto next ticket
-// EXERCISE !!!!
-// ADD ANOTHER FORM FIELD AND GET IT WORKING
-// PROVE TO MYSELF I CAN GET IT WORKING
-// SO I UNDERSTAND IT
-// ADD ANOTHER FIELD
-
-// TWO DIRECTIONS
-// BE HAPPY THAT I A FORM AND MAKE IT LOOK PRETTY
-// OR HANDLE SUBMISSION
-// when form is done add in time but how please how
